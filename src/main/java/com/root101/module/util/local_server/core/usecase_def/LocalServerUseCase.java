f5645cb0e43cf13c3b50d34210490fc9dea119f3
Unknown <unknown@example.com>
2021-01-08 17:59:28 -0500

    Licence a clases

--- src/main/java/com/root101/module/util/local_server/core/usecase_def/LocalServerUseCase.java
+++ src/main/java/com/root101/module/util/local_server/core/usecase_def/LocalServerUseCase.java
@@ -1,14 +1,28 @@
-package com.jhw.module.util.local_server.core.usecase_def;
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.root101.module.util.local_server.core.usecase_def;
 
 import com.root101.clean.core.app.usecase.ReadWriteUseCase;
-import com.jhw.module.util.local_server.core.domain.Configuration;
-import java.util.List;
+import com.root101.module.util.local_server.core.domain.Configuration;
 
 /**
- * Interfaz del caso de uso de la licencia para definir las principales
- * funcionalidades de la licencia
  *
- * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public interface LocalServerUseCase extends ReadWriteUseCase<Configuration> {
 
--- src/main/java/com/root101/module/util/local_server/core/usecase_impl/LocalServerUseCaseImpl.java
+++ src/main/java/com/root101/module/util/local_server/core/usecase_impl/LocalServerUseCaseImpl.java
@@ -1,19 +1,40 @@
-package com.jhw.module.util.local_server.core.usecase_impl;
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.root101.module.util.local_server.core.usecase_impl;
 
 import com.root101.clean.core.app.services.ExceptionHandler;
 import com.root101.clean.core.app.services.NotificationHandler;
 import com.root101.clean.core.app.services.NotificationsGeneralType;
 import com.root101.clean.core.app.usecase.DefaultReadWriteUseCase;
 import com.root101.clean.core.domain.services.ResourceHandler;
-import com.jhw.module.util.local_server.core.domain.Configuration;
-import com.jhw.module.util.local_server.core.module.LocalServerCoreModule;
+import com.root101.module.util.local_server.core.domain.Configuration;
+import com.root101.module.util.local_server.core.module.LocalServerCoreModule;
 import com.root101.utils.others.Network;
-import com.jhw.module.util.local_server.core.repo_def.LocalServerRepo;
-import com.jhw.module.util.local_server.core.usecase_def.LocalServerUseCase;
-import static com.jhw.module.util.local_server.core.utils.PreparedURLs.*;
+import com.root101.module.util.local_server.core.repo_def.LocalServerRepo;
+import com.root101.module.util.local_server.core.usecase_def.LocalServerUseCase;
+import static com.root101.module.util.local_server.core.utils.PreparedURLs.*;
 import java.util.concurrent.TimeoutException;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ *
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
+ */
 public class LocalServerUseCaseImpl extends DefaultReadWriteUseCase<Configuration> implements LocalServerUseCase {
 
     public static final String PROPERTY_STARTED = "rest_started";
